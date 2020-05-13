a5872eb686a333dc9305a01ff77ea063190073b3
Callofjohn <43967707+Callofjohn@users.noreply.github.com>
2020-05-12 17:11:24 -0700

    changing stuff

--- third report/public/testing.js
+++ third report/public/testing.js
@@ -1,27 +1,88 @@
-console.log("(1) testing.js is running fine");
+(function() {
+   function sendEmailLoginLink() {
+    a_send_link_btn.disabled = true
+    const  email = a_email.value
+    alert(email)
+  var actionCodeSettings = {
+    // URL you want to redirect back to. The domain (www.example.com) for this
+    // URL must be whitelisted in the Firebase Console.
+    url       : location.href ,
+              handleCodeInApp : true
+  }
+    
+    firebase.auth().sendSignInLinkToEmail(email, actionCodeSettings)
+    .then(function() {
 
-(function login() {
-    function login() {
-         const email = a_email.value
-         a_msg.innerText = "welcome john"
-         a_logging_in.style.display = "none"
-         a_logged_in.style.display = "block"
-         //alert(email)
-       }
+    a_msg.innerText = "Email has been sent to login in "
+    localStorage.setItem("email", email);
+       })
+
+       .catch(function(error) {
+         console.log(error)
+
+       })
+      }
+
+        function loginPageLoad() {
+          let email = localStorage.getItem("email")
+        if(!email)
+        {
+          email = prompt('PLease proivide your email for confirmation')
+        }
+        firebase.auth().signInWithEmailLink(email, location.href)
+        .then(function(result){
+          localStorage.removeItem("email")
+          //const user = result.user
+        })
+
+        .catch(function(error){
+          console.log(error)
+          
+        })
+    }
+         
+      
+
 
        function Logout () {
-         a_msg.innerText = "logged out"
-         a_logging_in.style.display = "block"
-         a_logged_in.style.display = "none"
+         
+         a__logout_btn.disabled = true
+         firebase.auth().signOut().catch(function(error)
+         {
+         console.log(error)
+        })
        }
+        
+
+      function authStateChanged(user)
+      {
+          if(user === null)
+          {
+              a_msg.innerHTML = "logged out"
+              a_logging_in.style.display = "block"
+              a_logged_in.style.display = "none"
+              a_send_link_btn.disable = false
+          } else {
+              a_msg.innerHTML = "logged in"
+              a_logging_in.style.display = "none"
+              a_logged_in.style.display = "block"
+              a__logout_btn.disable = false
+          }
+      }
+
+      
+
+
       document.addEventListener('DOMContentLoaded', () => {
-        document.getElementById("a_msg").innerHTML = "Signed in"
-        a_logging_in.style.display = "block"
-        a_send_link_btn.addEventListener('click', login);
-        a_logout_btn.addEventListener('click', Logout);
-       
-       
-        // firebase.auth().onAuthStateChanged(user => { });
-          //const app = firebase.app();
+        a_msg.innerHTML = "loaded"
+        a_send_link_btn.addEventListener('click', sendEmailLoginLink)
+        a_logout_btn.addEventListener('click', Logout)
+        const auth = firebase.auth()
+        auth.onAuthStateChanged(authStateChanged)
+        if (auth.isSignInWithEmailLink(location.href))
+        {
+            loginPageLoad()
+        }
+
         })
     })();
