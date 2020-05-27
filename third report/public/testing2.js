2946945b8cdaade18bfa815aea57d09bb5425588
Callofjohn <43967707+Callofjohn@users.noreply.github.com>
2020-05-27 13:09:43 -0700

    having fun

--- third report/public/testing2.js
+++ third report/public/testing2.js
@@ -15,16 +15,16 @@
         b_view_edit_btn.disabled = true
         b_view.style.display = "none"
         b_edit.style.display = "block"
-        b_edit.save.btn.disabled = false
+        b_edit_save_btn.disabled = false
         b_edit_cancel_btn.disabled = false
         })
 
         b_edit_cancel_btn.addEventListener('click', function() {
 
-            b_view_edit_btn.disabled = true
+            b_view_edit_btn.disabled = false
             b_view.style.display = "block"
             b_edit.style.display = "none"
-            b_edit.save.btn.disabled = true
+            b_edit_save_btn.disabled = true
             b_edit_cancel_btn.disabled = true
             })
         
@@ -34,7 +34,7 @@
             b_view_edit_btn.disabled = false
             b_view.style.display = "block"
             b_edit.style.display = "none"
-            b_edit.save.btn.disabled = true
+            b_edit_save_btn.disabled = true
             b_edit_cancel_btn.disabled = true
             })
 
